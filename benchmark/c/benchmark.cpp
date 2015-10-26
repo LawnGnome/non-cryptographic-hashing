90b0977b6af4df545c05416a53a3ca276b0bbabf
Adam Harvey <aharvey@php.net>
2015-10-26 09:18:57 +0100

    Add CRC32.

--- benchmark/c/benchmark.cpp
+++ benchmark/c/benchmark.cpp
@@ -6,6 +6,7 @@
 #include <sys/time.h>
 
 extern "C" {
+  uint32_t crc32(const char *data, size_t len, uint32_t ignored);
   uint32_t fnv1a32(const char *data, size_t len, uint32_t ignored);
   uint32_t murmurhash3_32(const char *data, size_t len, uint32_t seed);
   uint32_t xxhash32(const char *data, size_t len, uint32_t seed);
@@ -19,6 +20,7 @@ typedef struct {
 } algorithm_function;
 
 algorithm_function algorithms[] = {
+  {"crc32", crc32},
   {"fnv1a32", fnv1a32},
   {"murmurhash3", murmurhash3_32},
   {"xxhash32", xxhash32},
